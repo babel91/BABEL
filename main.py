ed08fab5d20c59e8849075df660f6628c01dfb5e
Babel <m.adolfsson91@gmail.com>
2023-03-21 14:03:08 +0100

    FU

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+
 import random
 import settings
 import discord
@@ -24,19 +25,52 @@ class Slapper(commands.Converter):
             nickname = ctx.author
         return f"{nickname} slaps {someone} with {argument}"
 
-def run():
+
+def run(): #Commands
     intents = discord.Intents.default()
     intents.message_content = True 
 
-    bot = commands.Bot(command_prefix="!", intents=intents)
+    bot = commands.Bot(command_prefix=".", intents=intents)
     
     @bot.event
     async def on_ready():
-        
+
         logger.info(f"User {bot.user}(ID: {bot.user.id})")
 
         channel = bot.get_channel(int(settings.CH_BOTS))
         await channel.send("Hello, I'm back!"),
+    
+    @bot.command(
+            aliases=['p'],
+            help = "This is hälp",
+            description = "This is description",
+            brief = "Short Explained",
+            enabled = True,
+            hidden = False
+    )
+    async def ping(ctx):
+        """Answers with pong"""
+        await ctx.send("pong")
+
+    @bot.command()
+    async def say(ctx, what = "WHAT?!"):
+        """WHAT?!"""
+        await ctx.send("what")
+
+    @bot.command()
+    async def say2(ctx, *what):
+        """what"""
+        await ctx.send(" ".join(what))
+
+    @bot.command()
+    async def say3(ctx, what = "WHAT?!", why = "WHY?"):
+        """WHAT?! WHY?"""
+        await ctx.send(what + why)
+
+    @bot.command()
+    async def choises(ctx, *options):
+        """Random shit"""
+        await ctx.send(random.choice(options))
     
     @bot.command()
     async def add(ctx, one : int , two : int):
@@ -45,16 +79,43 @@ def run():
     
     @bot.command()
     async def joined(ctx, who : discord.Member):
-        """Adds two numbers"""
+        """Who joined when?"""
         await ctx.send(who.joined_at)
 
     @bot.command()
     async def slap(ctx, tool : Slapper(use_nicknames=False)):
         await ctx.send(tool)
+    
+    @bot.command()
+    async def clear(ctx, amount=5):
+        """Clears given number of messages"""
+        await ctx.channel.purge(limit=amount+1)
 
 
+    @bot.event
+    async def on_ready():
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+
+    @bot.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("handled error globally")
+
+    @bot.event
+    async def add(ctx, one : int , two : int):
+        await ctx.send(one + two)
+
+    # @bot.error
+    # async def add_error(ctx, error):
+    #     if isinstance(error, commands.MissingRequiredArgument):
+    #         await ctx.send("handled error locally")
+
+
+    
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+
 
 if __name__ == "__main__":
     run()
+
 
